d0e7c083c28ebbb1623544807bd5dc6b243a0860
Amirmahdi <113615502+Amirmahdi-80@users.noreply.github.com>
2023-08-26 13:42:46 +0330

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,6 @@
+function fun1(){
+    document.getElementById("No1").classList.add("active")
+    document.getElementById("No2").classList.add("active")
+    document.getElementById("Par").classList.add("active")
+    document.getElementById("No4").classList.add("active")
+}
